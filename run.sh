32a885976e11ea65a612cb438509b082c1515da1
Rene Rhodes <rene.rhodes@gmail.com>
2017-03-25 21:23:31 -0700

    Repaired run.sh script and updated config.properties.

--- run.sh
+++ run.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 
-#arg1=$1
-#arg2=$2
+java -cp target/spreadsheet-1.0-SNAPSHOT.jar com.rhodes.spreadsheet.Launcher
 
-##directory where jar file is located    
-dir=./target/
-
-##jar file name
-jar_name=spreadsheet-1.0.-SNAPSHOT.jar
-
-## Perform some validation on input arguments, one example below
-#if [ -z "$1" ] || [ -z "$2" ]; then
-#        echo "Missing arguments, exiting.."
-#        echo "Usage : $0 arg1 arg2"
-#        exit 1
-#fi
-
-java -jar $dir/$jar_name com.rhodes.spreadsheet.Launcher
